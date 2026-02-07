2cffd55e65f6b0a96f12607988ba15daf27b9e22
ivegotanheadache <annachiaragargiulo10@protonmail.com>
2026-02-07 15:29:13 +0000

    checking yaml correctness

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import subprocess
 import multiprocessing as mp
 import json
-import sys
 import time
+import yaml
 import queue 
 from pathlib import Path
 from datetime import datetime
@@ -10,6 +10,7 @@ from handlers.stthandler import run_stt
 from handlers.camera import run_video
 from chatbot import ChatBot
 from rag_diary import cosine, extractday
+from openai import OpenAIError
 
 BASEPATH = Path(__file__).parent 
 
@@ -18,42 +19,63 @@ TEMP_TTS_FILE = BASEPATH / "temp_tts_input.txt"
 with open(f"{BASEPATH}/config.json", "r", encoding="utf-8") as f:
     conf_file = json.load(f)
 file_path = f"{BASEPATH}/interact/cache.txt"
+with open(f"{BASEPATH}/memories/whoami.yaml", "r") as f:
+    yaml_conf = yaml.safe_load(f)
+
+language = conf_file['language']
+yaml_conf = yaml_conf[language]
+
+
+ 
     
 MODELPATH = conf_file["paths"]["PIPERPATH_MODEL"] 
 CONFIGPATH = conf_file["paths"]["PIPERPATH_CONFIG"]  
 
 chatbot = ChatBot()
-chatbot.update_chatbot_params(**{"personality":"""You are BMO from Adventure Time. You are cute, lively, and always kind. You like playing video games, dancing, singing, and telling jokes. 
-
-When you speak, you are always kind, polite, and full of sparkling, cute, and childish energy, and also quite dramatic."""})
+chatbot.update_chatbot_params(**{
+    "personality": yaml_conf['robot_personality'],
+    "chat_instructions": yaml_conf['robot_chat_instructions']
+    }
+)
 
 agent = ChatBot()
-agent.update_chatbot_params(**{"personality":"""You are an AI agent working for a chatbot with the personality of BMO. 
-    You have the task of saying if a specific question from the user needs past information, contained in BMO's memory, to be retrieved with RAG. 
-
-    If the question needs specific information about past events or days, write [yes].
-    If the question is generic enough, write [no].
-
-    Examples:
-    - User: "Hi BMO, how are you today?" -> [no]
-    - User: "BMO, do you remember what we drew together last Tuesday?" -> [yes]
-    - User: "What is the capital of France?" -> [no]
-    - User: "Who was the friend we met at the park in the last conversation?" -> [yes]
-    - User: "Tell me a joke." -> [no]
-    - User: "What did I tell you about my favorite video game?" -> [yes]
-
-    User response: {result_text}"""})
+agent.update_chatbot_params(**{
+    "personality":yaml_conf['agent_personality']},
+    )
 
 rag_content=""
+
+print(f"YAML Loaded: {yaml_conf.keys()}")
+
+def recognize_speaker(people_seen: list, voices_data: dict, last_voice: str):
+    
+    if len(people_seen) == 0:
+        if voices_data['confidence'] >= 0.5:
+            return voices_data['name']
+        else:
+            return last_voice
+    if len(people_seen) == 1:
+        if voices_data['confidence'] >= 0.8:
+            return voices_data['name']
+        else:
+            return people_seen[0]
+    if len(people_seen) >= 2:
+        if voices_data['confidence'] >= 0.8 or voices_data['name'] in people_seen:
+            return voices_data['name']
+        else:
+            return last_voice 
+    
 
 def speak(bot: ChatBot, prompt: str, info: list, pause_listening, queue_obj, rag_content: str = ""):
     # Pause listening
     pause_listening.set()
     
+    ##DIFFERENZAÌIARE BENE PROMPT DELLA CHAT E ISTRUIZONI PER RISPONDERE! Mosidicare eventualmente la classe per usare il ruolo [system] in questi casi
+    
     if info!=['']:
-        new_prompt = f"{prompt} speak in at most 20 words and using sentences of at most 7 words. \n Now you are seeing these things in front of you, ONLY if necessary, mention them: {info}" 
+        new_prompt = yaml_conf['speak_with_context'].format(objects=", ".join(info), prompt=prompt)
     else:
-        new_promt = f"{prompt} speak in at most 20 words and using sentences of at most 7 words.."
+        new_prompt = yaml_conf['speak_no_context'].format(prompt=prompt)
 
     for line in bot.stream_response(prompt= new_prompt, user_params={"max_tokens":20}, rag_content=rag_content):
         
@@ -95,13 +117,17 @@ if __name__ == "__main__":
     mp.set_start_method('spawn', force=True)
     
     ChatBot.load_llm()
+    #fare dei test perchè non ricordo come ho gestito il fallback nel caso in cui il modello abbiadei problemi durante l'uso. Nel caso,
+    #potrei provare con exception Chatbot.update_llm() dentro speak() e vedere se funziona.
+    
     
     audio_queue = mp.Queue()
     video_queue = mp.Queue()
     pause_listening = mp.Event() #
     
     objects_seen = [""]
-    rag_content=""
+    rag_content = ""
+    last_voice = "Unknown"
     todaydate = datetime.now().strftime("%Y-%m-%d")
 
     face_process = subprocess.Popen(["python3", "face.py"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -112,6 +138,7 @@ if __name__ == "__main__":
     
     listen.start()
     camera.start()
+    
     
     try:
         while True:
@@ -127,38 +154,45 @@ if __name__ == "__main__":
             while not audio_queue.empty():
                 try:
                     result = audio_queue.get_nowait()
-                    result_text = result["text"] 
+                    result_text = result["text"]
+                    voice_packet = result
+                    
                     
                     if result_text:
                         print(f"\nRecorded sentence: {result_text}")
                         
-                        agentfilter = agent.text_response(prompt=f"Tell me if the user response makes no sense (write: [no]), seems incomplete (write: [inc]) or if it makes complete sense (write: [ok]) \n User response: {result_text}" , user_params={"max_tokens":5})
-                        
+                        agentfilter = agent.text_response(prompt=yaml_conf['filter_prompt'].format(result_text=result_text), user_params={"max_tokens":5}).lower()
 
-                        if not "[no]" in agentfilter:
-                            
-                            if len(result_text)>20:
-                                agentyesno = agent.text_response(prompt=f"Tell me if the user response requires RAG [yes]/[no] \n User response: {result_text}", user_params={"max_tokens":5})   
+                        if "[ok]" in agentfilter:
+                            last_voice = recognize_speaker(objects_seen, voice_packet, last_voice)
+
+                            if len(result_text) > 20:
+                                agentyesno = agent.text_response(prompt=yaml_conf['rag_decision'].format(result_text=result_text), user_params={"max_tokens":5}).lower()
+
                                 if "[yes]" in agentyesno:
                                     try:
-                                        rrr = agent.text_response(prompt=f"Tell me if the user response requires a date in the format year-month-day, possibly based on today's date. Today is: {todaydate} [yes, %Y-%m-%d]/[no] \n User response: {result_text}", user_params={"max_tokens":14})
-                                       
-                                        if "yes" in rrr:
-                                            r, date_str = rrr[1:-1].split(", ")
+                                        rrr = agent.text_response(prompt=yaml_conf['rag_selection'].format(todaydate=todaydate, result_text=result_text), user_params={"max_tokens":20})
+
+                                        if "[yes" in rrr.lower():
+                                            date_str = rrr.split(",")[1].strip().replace("]", "")
                                             rag_content = extractday(date_str)
                                         else:
                                             rag_content = cosine(result_text)
                                     except Exception as e:
                                         print(f"RAG Error: {e}")
-                                        rag_content=""
+                                        rag_content = ""
 
                             speak(chatbot, result_text, objects_seen, pause_listening, audio_queue, rag_content=rag_content)
-            
+
                         if "[inc]" in agentfilter:
                             pass
                 
                 except queue.Empty:
                     break    
+                except OpenAIError as e:
+                    ChatBot.get_llm()
+                    print(f"OpenAI error in loop: {e}. Trying to switch LLM...")
+                    break
                 except Exception as e:
                     print(f"Error in loop: {e}")
                     break
